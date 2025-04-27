b55b3fbcb7721acf8cafe4ef6064cf15e4dbc53f
kornej8 <deluxehaha@gmail.com>
2025-04-27 20:38:25 +0300

    to internet!

--- main.py
+++ main.py
@@ -5,49 +5,49 @@ from app.core.main_page import MainPage
 from app.core.change_page import ChangePage, WateringPage, WateringSavePage
 from db.db_connector import DBConnect
 
-app = Flask("flowers_website")
+application = Flask("flowers_website")
 db = DBConnect()
 
 
-@app.route('/')
+@application.route('/')
 def main():
     return MainPage(db, render_template=render_template)
 
 
-@app.route('/search/', methods=['GET'])
+@application.route('/search/', methods=['GET'])
 def search():
     flower_name = request.args.get('flower_name')
     return redirect(url_for('flower_page', flower_name=flower_name))
 
 
-@app.route('/<string:flower_name>')
+@application.route('/<string:flower_name>')
 def flower_page(flower_name):
     return MainPage(db=db, render_template=render_template, filter=flower_name)
 
 
-@app.route('/add_new_flower')
+@application.route('/add_new_flower')
 def add_new_flower():
     return AddFlowerPage(db=db, render_template=render_template)
 
 
-@app.route('/add_new_flower_post', methods=['POST'])
+@application.route('/add_new_flower_post', methods=['POST'])
 def add_new_flower_post():
     return GetNewFlower(db=db, render_template=render_template, request=request)
 
 
-@app.route('/changes')
+@application.route('/changes')
 def changes_page():
     return ChangePage(db=db, render_template=render_template)
 
 
-@app.route('/watering')
+@application.route('/watering')
 def watering_page():
     return WateringPage(db=db, render_template=render_template)
 
-@app.route('/watering', methods=['POST'])
+@application.route('/watering', methods=['POST'])
 def watering_page_post():
     return WateringSavePage(db=db, render_template=render_template, request=request)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
     # app.run(host="192.168.1.10", port=5000, debug=False)
