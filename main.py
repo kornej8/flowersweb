6375acf97c34e33eecc4b2d7bd3f1ae120097e68
kornej8 <deluxehaha@gmail.com>
2025-04-27 20:45:37 +0300

    to internet!

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ db = DBConnect()
 
 @application.route('/')
 def main():
-    return MainPage(db, render_template=render_template)
+    return 'Привет малышка кисулька)'
+    # return MainPage(db, render_template=render_template)
 
 
 @application.route('/search/', methods=['GET'])
