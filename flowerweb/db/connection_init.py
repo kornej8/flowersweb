17e62f98fc9cb69d16a1871b8035da1644cc5b5f
kornej8 <deluxehaha@gmail.com>
2025-05-03 17:00:58 +0300

    refactor addr

--- flowerweb/db/connection_init.py
+++ flowerweb/db/connection_init.py
@@ -6,6 +6,12 @@ class ConnectionInit:
         self.url = ConnectionInit.get_url(config, by=self.__connection_keyword)
 
     @staticmethod
+    def build_addr(host, port):
+        if port:
+            return f"{host}:{port}"
+        return host
+
+    @staticmethod
     def get_url(config, by, db='postgresql'):
 
         config_by=config.get(by, {})
@@ -15,12 +21,17 @@ class ConnectionInit:
         password = config_by.get('password')
         db_name = config_by.get('db_name')
 
-        url_string = "{db}://{user}:{password}@{host}:{port}/{db_name}".format(
+
+        addr = ConnectionInit.build_addr(host, port)
+
+        url_string = "{db}://{user}:{password}@{addr}/{db_name}".format(
             db=db,
             user=user,
-            host=host,
-            port=port,
+            addr=addr,
             password=password,
             db_name=db_name
         )
+
+        print(url_string)
+
         return url_string
--- flowerweb/migration.sql
+++ flowerweb/migration.sql
@@ -6,10 +6,31 @@ create table if not exists flowers_core.flowers (
 	dt timestamp default current_timestamp
 );
 
-
-CREATE TABLE flowers_core.flower_pictures (
+CREATE TABLE if not exists flowers_core.flower_pictures (
     id serial,
     flower_file_name text not null,
     flower_file bytea not null,
     dt timestamp default current_timestamp
 );
+
+alter table flowers_core.flower_pictures
+add column flower_file_square bytea;
+
+
+ALTER TABLE flowers_core.flowers ADD PRIMARY key (id);
+
+
+alter table flowers_core.flower_pictures
+drop column flower_file_square;
+
+ALTER TABLE flowers_core.flower_pictures ADD CONSTRAINT fk_name FOREIGN KEY (id) REFERENCES flowers_core.flowers(id);
+
+
+create table flowers_core.flowers_watering (
+	id serial,
+	watering_date date,
+	prev_watering_date date,
+	dt timestamp default current_timestamp
+);
+
+ALTER TABLE flowers_core.flowers_watering ADD CONSTRAINT fk_name FOREIGN KEY (id) REFERENCES flowers_core.flowers(id);
